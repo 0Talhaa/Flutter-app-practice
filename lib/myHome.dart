816061ad10153aff8c898a077831177c4e497b78
0Talhaa <mirzakhan4581207talha@gmail.com>
2025-01-24 08:08:59 +0500

    Flutter practice to be continue

--- lib/myHome.dart
+++ lib/myHome.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:talha/practice.dart';
 
 class Myhome extends StatefulWidget {
   const Myhome({super.key});
@@ -11,7 +14,78 @@ class _MyhomeState extends State<Myhome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text(
+          "WEBTECH",
+          style: TextStyle(
+            fontFamily: 'CustomFont', // Replace with your font family name
+            fontSize: 20, // Optional: Adjust font size
+            fontWeight: FontWeight.bold, // Optional: Set font weight
+          ),
+        ),
+        leading: Icon(Icons.menu),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyNavigate()),
+              );
+            },
+            child: Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Icon(
+              Icons.home_rounded,
+              // color: Color.fromARGB(255, 30, 30, 30),
+            ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyNavigate()),
+              );
+            },
+            child: Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Icon(
+              Icons.person_4_rounded,
+              // color: Color.fromARGB(255, 30, 30, 30),
+            ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyNavigate()),
+              );
+            },
+            child: Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Icon(
+              Icons.shopping_bag,
+              // color: Color.fromARGB(255, 30, 30, 30),
+            ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyNavigate()),
+              );
+            },
+            child: Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Icon(
+              Icons.search_rounded,
+              // color: Color.fromARGB(255, 30, 30, 30),
+            ),
+            ),
+          ),
+        ],
+        // backgroundColor: Colors.cyan,
+        // foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+        centerTitle: true,
+      ),
     );
   }
 }
---
+++ lib/practice.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyNavigate extends StatefulWidget {
+  const MyNavigate({super.key});
+
+  @override
+  State<MyNavigate> createState() => _MyNavigateState();
+}
+
+class _MyNavigateState extends State<MyNavigate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(destinations: const[
+        NavigationDestination(icon: Icon(Icons.home_mini_rounded), label: 'Home',),
+        NavigationDestination(icon: Icon(Icons.shop), label: 'Store',),
+        NavigationDestination(icon: Icon(Icons.heart_broken), label: 'WishList',),
+        NavigationDestination(icon: Icon(Icons.person), label: 'Profile',),
+      ]),
+    );
+  }
+}
